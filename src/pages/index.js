7e59685f76bc1fb8fbf0f780c90010e2c158f508
Kirill Zuev <mrrezak555@yandex.ru>
2023-01-06 17:09:50 +0300

    Pr8_2_1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js'
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
-import {popupElementEdit, popupAddElement, popupProfileOpenButton, popupAddCardButton, nameFormPopupElement, jobFormEPopuplement, newNamePopupCard, newImagePopupCard, nameElement, jobElement, popupElementPhoto, popupElementPhotoClose, selectors} from '../utils/constants.js';
+import {popupProfileOpenButton, popupAddCardButton, nameFormPopupElement, jobFormEPopuplement, selectors} from '../utils/constants.js';
 import './index.css';
 
 
